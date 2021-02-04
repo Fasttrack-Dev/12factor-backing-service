256e6067e05347cf19a6cdc197f0130bbef47d6f
Martin Keßler <martin.kessler@mhp.com>
2021-02-04 13:17:04 +0100

    Add dummy service

--- src/main/java/backing/BackingController.java
+++ src/main/java/backing/BackingController.java
@@ -1,5 +1,8 @@
 package backing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +13,30 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class BackingController {
 
+    Logger logger = LoggerFactory.getLogger(BackingController.class);
+
+    @Value("#{environment.BACKING_SERVICE_ENDPOINT}")
+    private String endpoint;
+
+    @Value("#{environment.BACKING_SERVICE_ENDPOINT_FALLBACK}")
+    private String endpoint_fallback;
+
+    private boolean useFallback = false;
+
     @GetMapping("/currency")
     @ResponseStatus(HttpStatus.OK)
     public String getCurrency() {
 
         RestTemplate restTemplate = new RestTemplate();
         String resourceUrl
-                = "https://api.frankfurter.app/latest";
+                = useFallback ? endpoint_fallback : endpoint;
+        useFallback = !useFallback;
+        if (resourceUrl == null) {
+            return "Backing service is not cofigured correctly.";
+        }
+        logger.info("Effective backing service endpoint is {}", resourceUrl);
         ResponseEntity<String> response
-                = restTemplate.getForEntity(resourceUrl , String.class);
+                = restTemplate.getForEntity(resourceUrl, String.class);
         return response.getBody();
     }
 }
